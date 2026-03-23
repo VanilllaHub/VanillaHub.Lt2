2b662bc3aeacdd8f0091f94a825aa7351176fa94
VanilllaHub <nielsenemma361@gmail.com>
2026-03-23 05:16:11 +0100

    Update Vanilla5.lua

--- Vanilla5.lua
+++ Vanilla5.lua
@@ -830,7 +830,7 @@ local function setBridge(lower)
         local lift = workspace.Bridge.VerticalLiftBridge.Lift
         for _, v in next, lift:GetChildren() do
             if v:IsA("BasePart") then
-                v.CFrame = v.CFrame + Vector3.new(0, lower and -0.1 or 0.1, 0)
+                v.CFrame = v.CFrame + Vector3.new(0, lower and -0.5 or 0.5, 0)
             end
         end
     end)
